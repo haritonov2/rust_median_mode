c932de244a060cc0409a03d48b77752096dc6bc6
haritonov2 <a.haritonov.web@gmail.com>
2024-08-31 09:45:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rust_median_mode
+A Rust solution for computing the median and mode of integer lists.
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub fn list_median(numbers: Vec<i32>) -> i32 {
+    // TBD
+
+    0
+}
+
+pub fn list_mod(numbers: Vec<i32>) -> i32 {
+    // TBD
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+
+}
+
+
+
